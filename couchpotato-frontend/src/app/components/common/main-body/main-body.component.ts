64c83369c7356e88f46f37157523c239a6707049
faysalsaber <faysal.saber@gmail.com>
2021-05-05 10:18:48 +0200

    frontend deployment

---
+++ couchpotato-frontend/src/app/components/common/main-body/main-body.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+/** Component used to display the main content of the website. */
+@Component({
+  selector: 'app-main-body',
+  templateUrl: './main-body.component.html',
+  styleUrls: ['./main-body.component.scss']
+})
+export class MainBodyComponent {
+
+}
