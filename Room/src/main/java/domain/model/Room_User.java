f82a7a43bae1b6acb02328834864f392e11bea7c
Akouete Sedoh <Akouete.Sedoh@etu.unige.ch>
2021-05-13 13:13:25 +0200

    Fix bugs in databases

--- Room/src/main/java/domain/model/Room_User.java
+++ Room/src/main/java/domain/model/Room_User.java
@@ -2,21 +2,20 @@ package domain.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 import java.util.Date;
 
 @Data
 @Entity
 @Table(name ="Room_User")
-public class Room_User {
+public class Room_User implements Serializable {
 
 	@Id
-	private int userId;
-	@NotNull
 	private int roomId;
+	@Id
+	private int userId;
 	@NotNull
 	private Date creationDate;
 
--- Room/src/main/resources/META-INF/room_tests_data.sql
+++ Room/src/main/resources/META-INF/room_tests_data.sql
@@ -1,5 +1,24 @@
+-- Room table
 TRUNCATE TABLE Room;
 INSERT INTO Room (roomId, roomAdminId, roomClosed) VALUES ( 1, 1, FALSE );
-INSERT INTO Room (roomId, roomAdminId, roomClosed) VALUES ( 2, 2, FALSE );
-INSERT INTO Room (roomId, roomAdminId, roomClosed) VALUES ( 3, 1, FALSE );
-INSERT INTO Room (roomId, roomAdminId, roomClosed) VALUES ( 4, 1, TRUE );
+INSERT INTO Room (roomId, roomAdminId, roomClosed) VALUES ( 2, 6, FALSE );
+INSERT INTO Room (roomId, roomAdminId, roomClosed) VALUES ( 3, 3, FALSE );
+INSERT INTO Room (roomId, roomAdminId, roomClosed) VALUES ( 4, 2, TRUE );
+
+-- Room_User table
+ALTER TABLE Room_User DROP PRIMARY KEY;
+ALTER TABLE Room_User ADD PRIMARY KEY (roomId, userId);
+TRUNCATE TABLE Room_User;
+INSERT INTO Room_User (roomId, userId, creationDate) VALUES ( 1, 1, CURRENT_TIMESTAMP );
+INSERT INTO Room_User (roomId, userId, creationDate) VALUES ( 1, 2, CURRENT_TIMESTAMP );
+INSERT INTO Room_User (roomId, userId, creationDate) VALUES ( 1, 3, CURRENT_TIMESTAMP );
+INSERT INTO Room_User (roomId, userId, creationDate) VALUES ( 2, 4, CURRENT_TIMESTAMP );
+INSERT INTO Room_User (roomId, userId, creationDate) VALUES ( 2, 5, CURRENT_TIMESTAMP );
+INSERT INTO Room_User (roomId, userId, creationDate) VALUES ( 2, 6, CURRENT_TIMESTAMP );
+INSERT INTO Room_User (roomId, userId, creationDate) VALUES ( 3, 3, CURRENT_TIMESTAMP );
+INSERT INTO Room_User (roomId, userId, creationDate) VALUES ( 4, 1, CURRENT_TIMESTAMP );
+INSERT INTO Room_User (roomId, userId, creationDate) VALUES ( 4, 2, CURRENT_TIMESTAMP );
+INSERT INTO Room_User (roomId, userId, creationDate) VALUES ( 4, 3, CURRENT_TIMESTAMP );
+INSERT INTO Room_User (roomId, userId, creationDate) VALUES ( 4, 4, CURRENT_TIMESTAMP );
+INSERT INTO Room_User (roomId, userId, creationDate) VALUES ( 4, 5, CURRENT_TIMESTAMP );
+INSERT INTO Room_User (roomId, userId, creationDate) VALUES ( 4, 6, CURRENT_TIMESTAMP );
