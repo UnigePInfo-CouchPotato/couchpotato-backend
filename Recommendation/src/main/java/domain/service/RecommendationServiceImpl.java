fa31f288ce4238fa1e452d501a3371d26bff5beb
AtomicNicos <atomicnicos@gmail.com>
2021-05-20 20:01:28 +0200

    Additional comments on the random top 5 by genres request, adding org.json to pom.xml

--- Recommendation/src/main/java/domain/service/RecommendationServiceImpl.java
+++ Recommendation/src/main/java/domain/service/RecommendationServiceImpl.java
@@ -1,12 +1,15 @@
-package domain.service;
+package domain.service;;
 
 import javax.enterprise.context.ApplicationScoped;
 import lombok.extern.java.Log;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import org.json.*;
 
 @ApplicationScoped
 @Log
@@ -32,7 +35,17 @@ public class RecommendationServiceImpl implements RecommendationService {
     }
 
     public String getAllFilmSelected(String idGenres){
-        // generate page num
+
+        // TODO check whether idGenres is in fact a string of numbers separated by ',' otherwise return status code 400
+        /*
+        STATUS CODE 400
+            {
+                success: false,
+                error: "Malformed request"
+            }
+        */
+
+        // TODO Remove sorting by votes here as it forces determinism in the response.
         String url = "https://api.themoviedb.org/3/discover/movie?api_key=b3299a1aa5ae43a9ae35cb544503117f&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&with_genres="+idGenres+"&with_watch_monetization_types=flatrate";
 
         Client client = ClientBuilder.newClient();
@@ -40,12 +53,36 @@ public class RecommendationServiceImpl implements RecommendationService {
 
         Response response = webTarget.request(MediaType.TEXT_PLAIN).header("result", 5).get();
 
-        // jsonStrin = response.getEntity()
-        // JsonObject json = JsonObject.fromString(jsonString)
+        // JSONObject json = new JSONObject(response.getEntity());
+        // JSONArray results = new JSONArray();
         
-        // order by field...
-        // get n first elements
-        // return that
+        // int maxPages = json.opt("total_pages");
+        // if maxPages is not null and is greater than 5
+        //      select 5 random unique numbers between 1 and maxPages
+        // else
+        //      take all page numbers from 1 to max_pages
+
+        // query each URL then build the JSONObject from the response
+        // Iterate over the JSONArray that is at: json.getJSONArray("results")
+        // Add each JSONObject within to results ^
+
+        // sort by vote_average (this implementation of Collections.sort would work: https://discourse.processing.org/t/sorting-a-jsonarray-by-one-of-its-values/4911/6)
+
+        // Take the first 5
+        // Return those in the format:
+        /*
+        STATUS CODE 200
+            {
+                success: true,
+                results: [
+                    { (movie details 1) },
+                    { (movie details 2) },
+                    { (movie details 3) },
+                    { (movie details 4) },
+                    { (movie details 5) }
+                ]
+            }
+         */
         
         if (response.getStatus() != 200) {
             return "Failed : HTTP error code : " + response.getStatus();
