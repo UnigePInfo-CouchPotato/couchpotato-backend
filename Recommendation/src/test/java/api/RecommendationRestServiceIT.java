15d0c107c729d2a51d87e5bb64ed001284e53063
ChristinaMarron <cng050996@gmail.com>
2021-06-05 23:43:25 +0200

    tests implemented

--- Recommendation/src/test/java/api/RecommendationRestServiceIT.java
+++ Recommendation/src/test/java/api/RecommendationRestServiceIT.java
@@ -1,16 +1,14 @@
 package api;
 
-import static io.restassured.RestAssured.when;
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.MatcherAssert.assertThat;
-
-import io.restassured.matcher.ResponseAwareMatcher;
-import io.restassured.response.Response;
-import org.apache.http.HttpStatus;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeAll;
-
 import io.restassured.RestAssured;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
+
 
 class RecommendationRestServiceIT {
 
@@ -19,16 +17,25 @@ class RecommendationRestServiceIT {
         RestAssured.baseURI = "http://localhost:28080/recommendation";
         RestAssured.port =8080;
     }
-
-
-
+    @Test
+    void testWelcomeMessage() {
+        when()
+                .get("/")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .body("service", equalTo("recommendation"))
+                .body("message", equalTo("Welcome!"));
+    }
 
     @Test
-    void testGetValidFilms() {
+    void testGetValidGenres() {
+
         when()
-                .get("/selectGenres=27,18")
+                .get("/genres")
                 .then()
-                .statusCode(HttpStatus.SC_OK);
+                .body("genres.id", hasItems(28,12,16,35,80,99,18,10751,14,36,27,10402,9648,10749,878,10770,53,10752,37));
     }
+
+
 
 }
