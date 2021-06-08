968e2da5140ca20bd34c055dbca18f60b608fba4
ChristinaMarron <cng050996@gmail.com>
2021-06-08 11:27:49 +0200

    probleme avec Contentype

--- Recommendation/src/test/java/api/RecommendationRestServiceIT.java
+++ Recommendation/src/test/java/api/RecommendationRestServiceIT.java
@@ -4,10 +4,14 @@ import io.restassured.RestAssured;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import javax.ws.rs.core.MediaType;
 
-import static io.restassured.RestAssured.when;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasItems;
+//import static io.restassured.RestAssured.when;
+//import static io.restassured.RestAssured.given;
+//import static org.hamcrest.Matchers.equalTo;
+//import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
 
 
 class RecommendationRestServiceIT {
@@ -33,8 +37,35 @@ class RecommendationRestServiceIT {
         when()
                 .get("/genres")
                 .then()
+                .statusCode(HttpStatus.SC_OK)
                 .body("genres.id", hasItems(28,12,16,35,80,99,18,10751,14,36,27,10402,9648,10749,878,10770,53,10752,37));
     }
+/*
+    @Test
+    public void testResponseForm() {
+        String num = "27";
+        given().
+                pathParam("idGenres", num).
+
+                when().
+                get("/selectGenres/{idGenres}").
+                then().
+                body(,lessThan(5));
+
+    }*/
+/*
+    @Test
+    public void testResponseForm() {
+        String num = "27";
+        given().
+                pathParam("idGenres", num).
+
+        when().
+                get("/selectGenres/{idGenres}").
+                then().
+                contentType(MediaType.APPLICATION_JSON);
+
+    }*/
 
 
 
